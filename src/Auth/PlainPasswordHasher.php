ef8cb7635e5bb88f450aaba9c49ea972a821ec2b
Percyboss <ing.percyboza@gmail.com>
2016-02-27 13:53:54 -0500

    cakephp

---
+++ src/Auth/PlainPasswordHasher.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Auth;
+
+use Cake\Auth\AbstractPasswordHasher;
+
+class PlainPasswordHasher extends AbstractPasswordHasher{
+
+    public function hash($password){
+//        return sha1($password);
+        return $password;
+    }
+
+    public function check($password, $hashedPassword){
+//        return sha1($password) === $hashedPassword;
+        return $password === $hashedPassword;
+    }
+}
+
+?>
